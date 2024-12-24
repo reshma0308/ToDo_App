4ec2e76b824e9e948a876aa95fe215668b59dae0
opal0822 <154756556+opal0822@users.noreply.github.com>
2024-12-25 01:40:08 +0530

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yes I am. Love you too.😍 Yayyyyyyyy</h1>
+    </div>
+  );
+}
+
+export default App;
